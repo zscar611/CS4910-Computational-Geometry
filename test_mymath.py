fc38ac99609518c312eca0c15398ecaa56c96c2b
Timothy Healey <87385121+timhealey3@users.noreply.github.com>
2024-02-27 10:28:18 -0500

    Update test_mymath.py

--- test_mymath.py
+++ test_mymath.py
@@ -7,3 +7,10 @@ def test_shortestdistance():
 
 def test_intersection():
 
+def text_convexHull():
+  dataPoints = [[2,4], [3,1], [4,7], [1,9], [10, 12]]
+  assert convexHUll() == [[3,1], [2, 4], [1,9], [10, 12]]
+  dataPoints = [[1,7], [2,3], [3,3], [4,9], [5, 13]]
+  assert convexHUll() == [[1,7], [2, 3], [3, 3], [5, 13]]
+  dataPoints = [[0,2], [2,2], [9,12], [13, 17]]
+  assert convexHUll() == [[0,2], [2, 2], [13,17]]
